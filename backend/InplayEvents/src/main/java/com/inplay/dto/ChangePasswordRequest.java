642cadf4d826d20ecf3fb8239fb593455a41ecbd
Luisma34 <luisma.cordon@gmail.com>
2026-02-20 20:07:38 +0000

    Hemos creado una nueva clase en dto

--- backend/InplayEvents/src/main/java/com/inplay/dto/ChangePasswordRequest.java
+++ backend/InplayEvents/src/main/java/com/inplay/dto/ChangePasswordRequest.java
@@ -1,4 +1,15 @@
 package com.inplay.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChangePasswordRequest {
+
+    // Contraseña actual (para validación)
+    private String passwordActual;
+
+    // Nueva contraseña
+    private String nuevaPassword;
 }
