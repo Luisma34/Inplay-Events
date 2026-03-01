49f3ce8e1b573ec4c320a3a721ce4ce6bd58e635
Luisma34 <luisma.cordon@gmail.com>
2026-03-01 10:21:45 +0000

    Cambios en anotaciones

--- backend/InplayEvents/src/main/java/com/inplay/security/SecurityConfig.java
+++ backend/InplayEvents/src/main/java/com/inplay/security/SecurityConfig.java
@@ -84,7 +84,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of("http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:8080"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
