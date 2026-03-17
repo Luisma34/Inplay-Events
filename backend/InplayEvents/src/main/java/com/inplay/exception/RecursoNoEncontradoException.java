d03cec2270de7e98fc7236b33c103f7d87289a64
Luisma34 <luisma.cordon@gmail.com>
2026-03-17 17:26:45 +0000

    rama para trabajar solo con front

--- backend/InplayEvents/src/main/java/com/inplay/exception/RecursoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.inplay.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException{
-
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
--- backend/InplayEvents/src/main/java/com/inplay/repository/PartidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inplay.repository;
-
-import com.inplay.entity.Partido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartidoRepository extends JpaRepository<Partido, Integer> {
-}
