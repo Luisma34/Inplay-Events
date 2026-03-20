2d887155e05f4463f17ae7badb18eca48dca4093
Luisma34 <luisma.cordon@gmail.com>
2026-03-20 18:46:53 +0000

    copia de backend from feature

--- backend/InplayEvents/src/main/java/com/inplay/controller/NoticiaController.java
+++ backend/InplayEvents/src/main/java/com/inplay/controller/NoticiaController.java
@@ -1,8 +1,10 @@
 package com.inplay.controller;
 
+import com.inplay.entity.Grupo;
 import com.inplay.entity.Noticia;
 import com.inplay.service.NoticiaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +18,12 @@ public class NoticiaController {
 
     private final NoticiaService noticiaService;
 
-    // Noticias públicas para que la puedan ver los usuarios no logueados
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public ResponseEntity<List<Noticia>> obtenerNoticias() {
         return ResponseEntity.ok(noticiaService.obtenerTodas());
     }
 
-    // Noticia individual pública
-    @GetMapping("{id}")
-    public ResponseEntity<Noticia> obtenerPorId(@PathVariable Integer id) {
-        return ResponseEntity.ok(noticiaService.obtenerPorId(id));
-    }
-
-    // Crear noticia -> solo admin
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERADMIN')")
     @PostMapping
     public ResponseEntity<Noticia> crear(@RequestBody Noticia noticia) {
@@ -36,17 +31,21 @@ public class NoticiaController {
         return ResponseEntity.status(201).body(creada);
     }
 
-    // Editar noticia -> solo admin
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("{id}")
+    public ResponseEntity<Noticia> obtenerPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(noticiaService.obtenerPorId(id));
+    }
+
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<Noticia> actualizar(
             @PathVariable Integer id,
-            @RequestBody Noticia noticia) {
+            @RequestBody Noticia noticia){
 
         return ResponseEntity.ok(noticiaService.actualizar(id, noticia));
     }
 
-    // Eliminar noticia -> solo admin
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
--- backend/InplayEvents/src/main/java/com/inplay/repository/NoticiaRepository.java
+++ backend/InplayEvents/src/main/java/com/inplay/repository/NoticiaRepository.java
@@ -4,6 +4,10 @@ import com.inplay.entity.Noticia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoticiaRepository extends JpaRepository<Noticia, Integer> {
+
+    List<Noticia> findByVisibleTrue();
 }
